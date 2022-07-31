b3b15af91b5abd17316da228eebd7fd393962f1d
SejinKim <32812045+PlutoSejin@users.noreply.github.com>
2022-07-31 13:02:21 +0900

    Create 1931.py

---
+++ Greedy/1931.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+time = []
+
+for _ in range(n):
+  time.append(list(map(int, input().split())))
+
+time = sorted(time, key=lambda a: a[0])
+time = sorted(time, key=lambda a: a[1])
+
+last = 0
+cnt = 0
+
+for i, j in time:
+  if i >= last:
+    cnt += 1
+    last = j
+
+print(cnt)
