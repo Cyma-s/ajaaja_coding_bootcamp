3e3e95ccf4e33a33fe864d7f5559e5269f1df03c
Cyma-s <ssol0319@naver.com>
2022-08-05 14:12:58 +0900

    add 9466

---
+++ eunsol/dfs/9466.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(120000)
+
+
+def dfs(x):
+    global result
+    visited[x] = True
+    cycle.append(x)
+    number = numbers[x]
+    if visited[number]:
+        if number in cycle:
+            result += cycle[cycle.index(number):]
+        return
+    else:
+        dfs(number)
+
+
+for _ in range(int(input())):
+    N = int(input())
+    numbers = [0] + list(map(int, input().split()))
+    visited = [True] + [False] * N
+    result = []
+    for i in range(1, N + 1):
+        if not visited[i]:
+            cycle = []
+            dfs(i)
+    print(N - len(result))
